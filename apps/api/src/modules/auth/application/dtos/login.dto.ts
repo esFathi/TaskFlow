4ec585023db88f881ba290df8ab7a70e7aad4bde
ehsanf <ehsaanfathiofficial@gmail.com>
2026-07-03 14:33:22 -0700

    ADD Login Dto

--- apps/api/src/modules/auth/application/dtos/login.dto.ts
+++ apps/api/src/modules/auth/application/dtos/login.dto.ts
@@ -1,1 +1,15 @@
 // Input DTO for login (email + password) with validation rules.
+
+import { IsEmail, IsString, Matches, MinLength } from "class-validator";
+
+export class LoginDto {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, {
+    message: "Password too weak",
+  })
+  password!: string;
+}
